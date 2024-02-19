7aceb2605319caa90a511d88a5dbe6c0685310e1
Kite Nguyen <kite.nguyen@pal.net.vn>
2024-02-19 15:56:08 +0700

    Add landing component

--- khuongnmdev/src/app/app.routes.ts
+++ khuongnmdev/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './home/landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: LandingComponent },
+  { path: '**', redirectTo: '/home' }
+];
