d4a8b1051f632862373e9c1c2b9bd0dc1721eaa2
Kite <kite.nguyen@pal.net.vn>
2024-09-30 18:51:08 +0700

    Test build GH

---
+++ docs/chunk-YCKJT5GV.js
@@ -0,0 +1,1 @@
+import{bb as a,cb as r,ea as o,pb as i,tb as s}from "./chunk-T77YE35D.js";var p=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["k-dashboard"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n, c){n&1&&(a(0,"p"),i(1,"dashboard works!"),r())}});let t=e;return t})();export{p as DashboardComponent};
