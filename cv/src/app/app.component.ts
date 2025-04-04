141fb8bcbcd938ddd4f68f27075c193617c26edb
khuongnm.dev <khuongnm.dev@gmail.com>
2025-04-05 06:20:06 +0700

    Add content for side bar

--- cv/src/app/app.component.ts
+++ cv/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBarComponent } from "./components/side-bar/side-bar.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,12 @@ import { HeaderComponent } from "./components/header/header.component";
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'cv';
+  constructor(
+    private readonly title: Title,
+    private readonly meta: Meta
+  ) {
+    this.title.setTitle('Khuong Nguyen Resume');
+    this.meta.addTag({ name: 'description', content: 'Resume of Khuong Nguyen' });
+  }
 }
+
